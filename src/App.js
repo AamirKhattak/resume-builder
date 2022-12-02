4f67948ee529e1fc4e4ed6e145ddf5d5723b2d8c
MrGreen <aamir.fui@gmail.com>
2022-12-02 15:54:44 +0500

    lodash

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import "./App.css";
 import { DMS_DATA } from "./util/dms-report";
+var _ = require('lodash');
 
 function App() {
   console.log(DMS_DATA);
+  const data =_.groupBy(_.sortBy(DMS_DATA, 'DEPARTMENT'), 'DEPARTMENT')
+  console.log(data);
   return <div className="App">{}</div>;
 }
 
