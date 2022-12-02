8fe016277b910fd741501181e72b11ed281adcd9
MrGreen <aamir.fui@gmail.com>
2022-12-02 15:44:44 +0500

    dms-report

--- src/App.js
+++ src/App.js
@@ -1,51 +1,8 @@
 import "./App.css";
-import { Resume } from "./util/resume-template";
+import { DMS_DATA } from "./util/dms-report";
 
 function App() {
-  const aamirResume = new Resume();
-
-  aamirResume.addPersonalInfo(
-    "https://media-exp1.licdn.com/dms/image/D4D03AQE2Fs9NrOOMxA/profile-displayphoto-shrink_200_200/0/1669964820953?e=1675296000&v=beta&t=3m-NbxjySzFt1d-rDtCCn6SBQWcxjivohfaKdqt05W8",
-    "https://media-exp1.licdn.com/dms/image/D4D03AQE2Fs9NrOOMxA/profile-displayphoto-shrink_200_200/0/1669964820953?e=1675296000&v=beta&t=3m-NbxjySzFt1d-rDtCCn6SBQWcxjivohfaKdqt05W8",
-    "Muhammad Aamir Javid",
-    "Muhammad Javid",
-    "0333-1231236",
-    "aamir.xyz.abc@gmail.com",
-    "https://www.linkedin.com/in/abc",
-    "https://github.com/AamirKhattak",
-    "Software Engineer by Choice!"
-  );
-
-  aamirResume.addSkills("JavaScript", "Current");
-  aamirResume.addSkills("MERN Stack", "Current");
-  aamirResume.addSkills("MySQL", "Current");
-  aamirResume.addSkills("PostgreSQL", "Current");
-  aamirResume.addSkills("C/C++ ", "Past");
-  aamirResume.addSkills("JAVA", "Past");
-  aamirResume.addSkills("Android Native", "Past");
-  aamirResume.addSkills("CUDA", "Past");
-  aamirResume.addSkills("OpenCV", "Past");
-  aamirResume.addSkills("Boost-C++", "Past");
-
-  aamirResume.addExperience(
-    "Assistant Engineer (Software)",
-    "full-time",
-    "OGDCL",
-    "Islamabad",
-    "true",
-    { month: "oct", year: "2017" },
-    undefined,
-    "Oil & Gas",
-    `Here I am responsible for the design and development of the following web application 
-  projects. 
-  ▪  Helpdesk Management Software 
-  ▪  Payroll Management Software 
-  ▪  Land Management Software`
-  );
-
-
-  console.log(aamirResume.resume);
-
+  console.log(DMS_DATA);
   return <div className="App">{}</div>;
 }
 
---
+++ src/util/dms-report.js
@@ -0,0 +1,299 @@
+export const DMS_DATA = [
+    {
+      "DEPARTMENT": "Communication",
+      "SUBSECTION": "Communication",
+      "DOC_COUNT": 47,
+      "YEAR": 2022,
+      "MONTHNAME": "November",
+      "MONTH": 11
+    },
+    {
+      "DEPARTMENT": "Communication",
+      "SUBSECTION": "Communication",
+      "DOC_COUNT": 39,
+      "YEAR": 2022,
+      "MONTHNAME": "October",
+      "MONTH": 10
+    },
+    {
+      "DEPARTMENT": "Corporate Affairs",
+      "SUBSECTION": "CA",
+      "DOC_COUNT": 6,
+      "YEAR": 2022,
+      "MONTHNAME": "October",
+      "MONTH": 10
+    },
+    {
+      "DEPARTMENT": "Drilling Fluid Services",
+      "SUBSECTION": "Drilling Fluid Services",
+      "DOC_COUNT": 3,
+      "YEAR": 2022,
+      "MONTHNAME": "November",
+      "MONTH": 11
+    },
+    {
+      "DEPARTMENT": "Drilling Fluid Services",
+      "SUBSECTION": "Drilling Fluid Services",
+      "DOC_COUNT": 14,
+      "YEAR": 2022,
+      "MONTHNAME": "October",
+      "MONTH": 10
+    },
+    {
+      "DEPARTMENT": "Exploration",
+      "SUBSECTION": "ECMP",
+      "DOC_COUNT": 2,
+      "YEAR": 2022,
+      "MONTHNAME": "November",
+      "MONTH": 11
+    },
+    {
+      "DEPARTMENT": "Exploration",
+      "SUBSECTION": "ECMP",
+      "DOC_COUNT": 2,
+      "YEAR": 2022,
+      "MONTHNAME": "October",
+      "MONTH": 10
+    },
+    {
+      "DEPARTMENT": "Exploration",
+      "SUBSECTION": "GS",
+      "DOC_COUNT": 428,
+      "YEAR": 2022,
+      "MONTHNAME": "November",
+      "MONTH": 11
+    },
+    {
+      "DEPARTMENT": "Exploration",
+      "SUBSECTION": "GS",
+      "DOC_COUNT": 1,
+      "YEAR": 2022,
+      "MONTHNAME": "October",
+      "MONTH": 10
+    },
+    {
+      "DEPARTMENT": "Exploration",
+      "SUBSECTION": "PG",
+      "DOC_COUNT": 101,
+      "YEAR": 2022,
+      "MONTHNAME": "October",
+      "MONTH": 10
+    },
+    {
+      "DEPARTMENT": "Exploration",
+      "SUBSECTION": "PG-South East",
+      "DOC_COUNT": 2,
+      "YEAR": 2022,
+      "MONTHNAME": "November",
+      "MONTH": 11
+    },
+    {
+      "DEPARTMENT": "Exploration (TDL)",
+      "SUBSECTION": "TDL",
+      "DOC_COUNT": 13,
+      "YEAR": 2022,
+      "MONTHNAME": "October",
+      "MONTH": 10
+    },
+    {
+      "DEPARTMENT": "Finance & Accounts",
+      "SUBSECTION": "Finance & Accounts",
+      "DOC_COUNT": 11,
+      "YEAR": 2022,
+      "MONTHNAME": "November",
+      "MONTH": 11
+    },
+    {
+      "DEPARTMENT": "Finance & Accounts",
+      "SUBSECTION": "Finance & Accounts",
+      "DOC_COUNT": 7,
+      "YEAR": 2022,
+      "MONTHNAME": "October",
+      "MONTH": 10
+    },
+    {
+      "DEPARTMENT": "HR",
+      "SUBSECTION": "HR - Policy",
+      "DOC_COUNT": 17,
+      "YEAR": 2022,
+      "MONTHNAME": "November",
+      "MONTH": 11
+    },
+    {
+      "DEPARTMENT": "HR",
+      "SUBSECTION": "HR - Policy",
+      "DOC_COUNT": 27,
+      "YEAR": 2022,
+      "MONTHNAME": "October",
+      "MONTH": 10
+    },
+    {
+      "DEPARTMENT": "HR",
+      "SUBSECTION": "HR(Ops)-E",
+      "DOC_COUNT": 135,
+      "YEAR": 2022,
+      "MONTHNAME": "November",
+      "MONTH": 11
+    },
+    {
+      "DEPARTMENT": "HR",
+      "SUBSECTION": "HR(Ops)-E",
+      "DOC_COUNT": 99,
+      "YEAR": 2022,
+      "MONTHNAME": "October",
+      "MONTH": 10
+    },
+    {
+      "DEPARTMENT": "HR",
+      "SUBSECTION": "HR(Ops)-G",
+      "DOC_COUNT": 18,
+      "YEAR": 2022,
+      "MONTHNAME": "October",
+      "MONTH": 10
+    },
+    {
+      "DEPARTMENT": "HR",
+      "SUBSECTION": "Pension",
+      "DOC_COUNT": 324,
+      "YEAR": 2022,
+      "MONTHNAME": "November",
+      "MONTH": 11
+    },
+    {
+      "DEPARTMENT": "HR",
+      "SUBSECTION": "Pension",
+      "DOC_COUNT": 184,
+      "YEAR": 2022,
+      "MONTHNAME": "October",
+      "MONTH": 10
+    },
+    {
+      "DEPARTMENT": "HSEQ",
+      "SUBSECTION": "HSEQ",
+      "DOC_COUNT": 4,
+      "YEAR": 2022,
+      "MONTHNAME": "November",
+      "MONTH": 11
+    },
+    {
+      "DEPARTMENT": "HSEQ",
+      "SUBSECTION": "HSEQ",
+      "DOC_COUNT": 109,
+      "YEAR": 2022,
+      "MONTHNAME": "October",
+      "MONTH": 10
+    },
+    {
+      "DEPARTMENT": "JV&BD",
+      "SUBSECTION": "JV&BD",
+      "DOC_COUNT": 9,
+      "YEAR": 2022,
+      "MONTHNAME": "November",
+      "MONTH": 11
+    },
+    {
+      "DEPARTMENT": "JV&BD",
+      "SUBSECTION": "JV&BD",
+      "DOC_COUNT": 61,
+      "YEAR": 2022,
+      "MONTHNAME": "October",
+      "MONTH": 10
+    },
+    {
+      "DEPARTMENT": "MD Secretariat",
+      "SUBSECTION": "MD Secretariat",
+      "DOC_COUNT": 288,
+      "YEAR": 2022,
+      "MONTHNAME": "November",
+      "MONTH": 11
+    },
+    {
+      "DEPARTMENT": "MD Secretariat",
+      "SUBSECTION": "MD Secretariat",
+      "DOC_COUNT": 368,
+      "YEAR": 2022,
+      "MONTHNAME": "October",
+      "MONTH": 10
+    },
+    {
+      "DEPARTMENT": "SCM",
+      "SUBSECTION": "SCM - Foreign - C",
+      "DOC_COUNT": 43,
+      "YEAR": 2022,
+      "MONTHNAME": "November",
+      "MONTH": 11
+    },
+    {
+      "DEPARTMENT": "SCM",
+      "SUBSECTION": "SCM - Foreign - C",
+      "DOC_COUNT": 35,
+      "YEAR": 2022,
+      "MONTHNAME": "October",
+      "MONTH": 10
+    },
+    {
+      "DEPARTMENT": "SCM",
+      "SUBSECTION": "SCM - Local",
+      "DOC_COUNT": 7,
+      "YEAR": 2022,
+      "MONTHNAME": "November",
+      "MONTH": 11
+    },
+    {
+      "DEPARTMENT": "SCM",
+      "SUBSECTION": "SCM - Local",
+      "DOC_COUNT": 24,
+      "YEAR": 2022,
+      "MONTHNAME": "October",
+      "MONTH": 10
+    },
+    {
+      "DEPARTMENT": "SCM",
+      "SUBSECTION": "SCM - Services",
+      "DOC_COUNT": 6,
+      "YEAR": 2022,
+      "MONTHNAME": "November",
+      "MONTH": 11
+    },
+    {
+      "DEPARTMENT": "SCM",
+      "SUBSECTION": "SCM - Services",
+      "DOC_COUNT": 5,
+      "YEAR": 2022,
+      "MONTHNAME": "October",
+      "MONTH": 10
+    },
+    {
+      "DEPARTMENT": "Systems",
+      "SUBSECTION": "IT Apps",
+      "DOC_COUNT": 78,
+      "YEAR": 2022,
+      "MONTHNAME": "November",
+      "MONTH": 11
+    },
+    {
+      "DEPARTMENT": "Systems",
+      "SUBSECTION": "IT Apps",
+      "DOC_COUNT": 84,
+      "YEAR": 2022,
+      "MONTHNAME": "October",
+      "MONTH": 10
+    },
+    {
+      "DEPARTMENT": "Systems",
+      "SUBSECTION": "IT Operations",
+      "DOC_COUNT": 82,
+      "YEAR": 2022,
+      "MONTHNAME": "November",
+      "MONTH": 11
+    },
+    {
+      "DEPARTMENT": "Systems",
+      "SUBSECTION": "IT Operations",
+      "DOC_COUNT": 51,
+      "YEAR": 2022,
+      "MONTHNAME": "October",
+      "MONTH": 10
+    }
+  ]
+  
